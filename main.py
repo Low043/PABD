4eb65944f1d1675211ab1cfb6c25ae4639110762
Luís Gustavo <luisgustavopereiraf@gmail.com>
2024-10-09 17:35:11 -0300

    Import Style Error Resolved

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ class NFTStore:
         self.feed = FeedView()
         self.cart = CartView()
 
+    def loadCSS(window):#Import default CSS to all Windows
+        window.load_css(open('src/css/style.css','r').read())
+
 #Init Login View
-webview.create_window("NFTStore", "src/views/login/index.html",js_api=NFTStore(),width=600,height=600,min_size=(600,600))
-webview.start()
+window = webview.create_window("NFTStore", "src/views/login/index.html",js_api=NFTStore(),width=600,height=600,min_size=(600,600))
+webview.start(NFTStore.loadCSS,window)
