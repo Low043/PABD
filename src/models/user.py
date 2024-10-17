6fa39046bb8e463e9fe51b9a52d23182b354aead
LUIS GUSTAVO PEREIRA FERNANDES <20221214010004@ifrn.local>
2024-10-17 17:41:40 -0300

    Começando do zero com um modeo feito no paint e um sonho

--- src/models/user.py
+++
@@ -1,5 +0,0 @@
-class User:
-    def __init__(self,ID,name,password):
-        self.ID = ID
-        self.name = name
-        self.password = password
--- src/views/__init__.py
+++
@@ -1,4 +0,0 @@
-from src.views.login import *
-from src.views.register import *
-from src.views.feed import *
-from src.views.cart import *
